8a7b5568c2eb3f19c8e8fe2427fa43fa38add777
jmcho2010 <projm2786@gmail.com>
2025-02-18 13:53:01 +0900

    tesT

--- src/JestExam/matchers.test.js
+++ src/JestExam/matchers.test.js
@@ -1,12 +1,10 @@
 //matchers.test.js
-import getUser from './test1';
+//import user from './test1';
+const { getUser } = require('./test1');
 
-describe('그냥 jest 써볼거임', () =>{
-    const { getUser } = require('user');
 
     test("user 객체 가져온 결과", () =>{
         expect(getUser(1)).toEqual({
             gunchim:"ssak"
         })
     });
-});
--- src/JestExam/test1.js
+++ src/JestExam/test1.js
@@ -6,4 +6,4 @@ function getUser(id){
     };
 }
 // 위의 함수는 컴포넌트가 아닌 모듈함수임을 선언.
-module.exports = getUser;
+module.exports = { getUser };
